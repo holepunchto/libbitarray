6fe321b18dc00503cadeb69507c1897bf5b3d698
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-06-04 10:45:31 +0200

    Add initial fuzz test

---
+++ test/fuzz/entry.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <bitarray.h>
+#include <quickbit.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int
+LLVMFuzzerTestOneInput (const uint8_t *data, size_t size) {
+  int err;
+
+  bitarray_t b;
+  err = bitarray_init(&b, NULL, NULL);
+  assert(err == 0);
+
+  err = bitarray_insert(&b, data, size, 0);
+  assert(err == 0);
+
+  int64_t len = size * 8, pos = 0;
+
+  while (pos < len) {
+    int64_t start = pos;
+
+    pos = bitarray_find_first(&b, true, start);
+
+    assert(quickbit_find_first((const quickbit_t) data, size, true, start) == pos);
+
+    if (pos == -1) break;
+
+    pos += 1;
+  }
+
+  bitarray_destroy(&b);
+
+  return 0;
+}
